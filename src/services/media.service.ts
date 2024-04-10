19d18e4a97f3fab9c30c37fd1d9d2d6cce892b6a
alexeal <chillncode@gmail.com>
2024-04-10 23:39:40 +0200

    Sort lists before returning in observables

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, of, tap } from 'rxjs';
 import { Media } from '../models/media.model';
-import PodcastsData  from "../data/podcasts.json";
-import VideosData  from "../data/videos.json";
+import PodcastsData from "../data/podcasts.json";
+import VideosData from "../data/videos.json";
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +12,23 @@ export class MediaService {
 
   constructor(private http: HttpClient) { }
 
+  private sortData(data : typeof PodcastsData | typeof VideosData): void {
+    if(JSON.stringify(data).length > 2) {
+      data.sort(function (a: any, b: any) {
+        return a.title && b.title && a.title.localeCompare(b.title);
+      });
+    }
+  }
+
   public getPodcasts(): Observable<Media> {
+    this.sortData(PodcastsData);
     return of(PodcastsData).pipe(
       map((res) => { return <Media><unknown>res; })
     );
   }
 
   public getVideos(): Observable<Media> {
+    this.sortData(VideosData);
     return of(VideosData).pipe(
       map((res) => { return <Media><unknown>res; })
     );
