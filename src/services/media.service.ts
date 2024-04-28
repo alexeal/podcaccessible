23dba455036e8f9b31515ffa55a73c838c69a925
alexeal <chillncode@gmail.com>
2024-04-28 10:58:46 +0200

    Refactoring data

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -2,8 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, of, tap } from 'rxjs';
 import { Media } from '../models/media.model';
-import PodcastsData from "../data/podcasts.json";
-import VideosData from "../data/videos.json";
+import MediasData from "../data/medias.json";
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +11,7 @@ export class MediaService {
 
   constructor(private http: HttpClient) { }
 
-  private sortData(data : typeof PodcastsData | typeof VideosData): void {
+  private sortData(data : typeof MediasData): void {
     if(JSON.stringify(data).length > 2) {
       data.sort(function (a: any, b: any) {
         return a.title && b.title && a.title.localeCompare(b.title);
@@ -20,16 +19,21 @@ export class MediaService {
     }
   }
 
+  private filterByMediaType(data : typeof MediasData, mediaType: string) {
+    return data.filter(obj => obj['mediaType'] === mediaType);
+  }
+
   public getPodcasts(): Observable<Media> {
-    this.sortData(PodcastsData);
-    return of(PodcastsData).pipe(
+    this.sortData(MediasData);
+    return of(this.filterByMediaType(MediasData, 'podcast')).pipe(
       map((res) => { return <Media><unknown>res; })
     );
   }
 
   public getVideos(): Observable<Media> {
-    this.sortData(VideosData);
-    return of(VideosData).pipe(
+    this.sortData(MediasData);
+    console.log(MediasData)
+    return of(this.filterByMediaType(MediasData, 'video')).pipe(
       map((res) => { return <Media><unknown>res; })
     );
   }
